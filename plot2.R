592221950ca3bd27a854fc295f584e1442adb367
aglevine <aglevine@wisc.edu>
2017-01-15 14:45:50 -0500

    added second plot and script

---
+++ plot2.R
@@ -0,0 +1,18 @@
+#Plot2.R: creates global active power histogram
+
+
+#######Get and clean data######
+housePower <- read.csv("household_power_consumption.txt",header=TRUE,sep=";",colClasses=c("character","character","character","character","character","character","character","character","numeric")) #get the data
+
+housePower <- subset(housePower,as.Date(housePower$Date,format="%d/%m/%Y") >= "2007-02-01" & as.Date(housePower$Date,format="%d/%m/%Y") <= "2007-02-02") #get the dates we want
+
+housePower <- transform(housePower, Global_active_power = as.numeric(Global_active_power), Global_reactive_power = as.numeric(Global_reactive_power),Voltage=as.numeric(Voltage),Global_intensity=as.numeric(Global_intensity),Sub_metering_1 = as.numeric(Sub_metering_1),Sub_metering_2 = as.numeric(Sub_metering_2),Sub_metering_3=as.numeric(Sub_metering_3)) #convert all but dates to numeric
+
+housePower <- subset(housePower,!is.na(Global_active_power) & !is.na(Global_reactive_power) & !is.na(Voltage) & !is.na(Global_intensity) & !is.na(Sub_metering_1) & !is.na(Sub_metering_2) & !is.na(Sub_metering_3)) #get rid of na's
+
+housePower <- within(housePower, {timestamp = strptime(paste(Date,Time),format="%d/%m/%Y %H:%M:%S")}) #add timestamp
+
+####make plot####
+png(filename="plot2.png")
+plot(housePower$timestamp,housePower$Global_active_power,'l',xlab="",ylab="Global Active Power (kilowatts)",main="")
+dev.off()
