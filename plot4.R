908aa0cc29efb7cb558c15742fea48c65fc8bd47
aglevine <aglevine@wisc.edu>
2017-01-15 16:31:02 -0500

    added comments about cleaning data

--- plot4.R
+++ plot4.R
@@ -2,15 +2,21 @@
 
 
 #######Get and clean data######
-housePower <- read.csv("household_power_consumption.txt",header=TRUE,sep=";",colClasses=c("character","character","character","character","character","character","character","character","numeric")) #get the data
 
-housePower <- subset(housePower,as.Date(housePower$Date,format="%d/%m/%Y") >= "2007-02-01" & as.Date(housePower$Date,format="%d/%m/%Y") <= "2007-02-02") #get the dates we want
+#get data
+housePower <- read.csv("household_power_consumption.txt",header=TRUE,sep=";",colClasses=c("character","character","character","character","character","character","character","character","numeric"))
 
-housePower <- transform(housePower, Global_active_power = as.numeric(Global_active_power), Global_reactive_power = as.numeric(Global_reactive_power),Voltage=as.numeric(Voltage),Global_intensity=as.numeric(Global_intensity),Sub_metering_1 = as.numeric(Sub_metering_1),Sub_metering_2 = as.numeric(Sub_metering_2),Sub_metering_3=as.numeric(Sub_metering_3)) #convert all but dates to numeric
+#get the dates we want
+housePower <- subset(housePower,as.Date(housePower$Date,format="%d/%m/%Y") >= "2007-02-01" & as.Date(housePower$Date,format="%d/%m/%Y") <= "2007-02-02")
 
-housePower <- subset(housePower,!is.na(Global_active_power) & !is.na(Global_reactive_power) & !is.na(Voltage) & !is.na(Global_intensity) & !is.na(Sub_metering_1) & !is.na(Sub_metering_2) & !is.na(Sub_metering_3)) #get rid of na's
+#convert all but dates to numeric
+housePower <- transform(housePower, Global_active_power = as.numeric(Global_active_power), Global_reactive_power = as.numeric(Global_reactive_power),Voltage=as.numeric(Voltage),Global_intensity=as.numeric(Global_intensity),Sub_metering_1 = as.numeric(Sub_metering_1),Sub_metering_2 = as.numeric(Sub_metering_2),Sub_metering_3=as.numeric(Sub_metering_3))
 
-housePower <- within(housePower, {timestamp = strptime(paste(Date,Time),format="%d/%m/%Y %H:%M:%S")}) #add timestamp
+#get rid of na's
+housePower <- subset(housePower,!is.na(Global_active_power) & !is.na(Global_reactive_power) & !is.na(Voltage) & !is.na(Global_intensity) & !is.na(Sub_metering_1) & !is.na(Sub_metering_2) & !is.na(Sub_metering_3))
+
+#add timestamp
+housePower <- within(housePower, {timestamp = strptime(paste(Date,Time),format="%d/%m/%Y %H:%M:%S")}) 
 
 ####make plot####
 png(filename="plot4.png")
